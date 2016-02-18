4020713473a6ba11476ceb51d49babaf57646e87
beyond <beyondblog@outlook.com>
2016-02-18 09:13:16 +0800

    add args

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	json "github.com/bitly/go-simplejson"
+	"github.com/codegangsta/cli"
 	"github.com/coreos/etcd/Godeps/_workspace/src/golang.org/x/net/context"
 	"github.com/coreos/etcd/client"
 	"github.com/vulcand/oxy/forward"
@@ -10,6 +11,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"time"
 )
 
@@ -21,7 +23,7 @@ func ParseURI(uri string) *url.URL {
 	return out
 }
 
-func main() {
+func StartProxy(port int, serviceName string, servicePort int) {
 
 	fwd, _ := forward.New()
 	lb, _ := roundrobin.New(fwd)
@@ -39,28 +41,74 @@ func main() {
 	}
 
 	kapi := client.NewKeysAPI(c)
-	resp, err := kapi.Get(context.Background(), "/registry/services/endpoints/default/{server-name}", nil)
+	resp, err := kapi.Get(context.Background(), "/registry/services/endpoints/default/"+serviceName, nil)
 	if err != nil {
+		log.Print("serverName not found!")
 		log.Fatal(err)
 	}
 	nodeJson, _ := json.NewJson([]byte(resp.Node.Value))
 
 	addresses, _ := nodeJson.Get("subsets").GetIndex(0).Get("addresses").Array()
 	for _, address := range addresses {
-		url := fmt.Sprintf("http://%s:8080", address.(map[string]interface{})["ip"].(string))
+		url := fmt.Sprintf("http://%s:%d", address.(map[string]interface{})["ip"].(string), servicePort)
 
 		lb.UpsertServer(ParseURI(url))
 	}
 
+	addr := fmt.Sprintf(":%d", port)
+
 	s := &http.Server{
-		Addr:    ":8888",
+		Addr:    addr,
 		Handler: lb,
 	}
 
-	log.Print("Listen port 8888")
+	log.Print("Listen " + addr)
 
 	if err := s.ListenAndServe(); err != nil {
 		log.Print("Listen Error!")
 	}
 
+}
+
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "k8s-router"
+	app.Usage = "Simple HTTP router for Kubernetes"
+	app.Version = "0.0.1"
+
+	var (
+		port        int
+		serviceName string
+		servicePort int
+	)
+
+	app.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:        "port, p",
+			Value:       8888,
+			Usage:       "proxy listen port",
+			Destination: &port,
+		},
+		cli.StringFlag{
+			Name:        "serviceName, s",
+			Usage:       "proxy kubernetes serviceName",
+			Destination: &serviceName,
+		},
+		cli.IntFlag{
+			Name:        "servicePort, sp",
+			Value:       8080,
+			Usage:       "proxy kubernetes service port",
+			Destination: &servicePort,
+		},
+	}
+
+	app.Action = func(c *cli.Context) {
+		if len(serviceName) == 0 {
+			log.Fatal("serverName is require!")
+		}
+		StartProxy(port, serviceName, servicePort)
+	}
+
+	app.Run(os.Args)
 }
